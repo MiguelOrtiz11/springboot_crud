7a6b7769f9f5e7a2ca5161de53ff2fe2a19da91e
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-04-26 14:59:31 -0500

    Volviendo a la estructura base del codigo

--- src/main/java/co/edu/usco/pw/springboot_crud01/controller/WelcomeController.java
+++ src/main/java/co/edu/usco/pw/springboot_crud01/controller/WelcomeController.java
@@ -13,7 +13,7 @@ public class WelcomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showWelcomePage(ModelMap model) {
 		model.put("name", getLoggedinUserName());
-		return "student";
+		return "welcome";
 	}
 
 	private String getLoggedinUserName() {
